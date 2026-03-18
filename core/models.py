b6cd1a9ebd8ae692a25b2656f249600b53107fcc
Kh-Tanjim-Ashraf <kh.tanjim.ashraf>
2026-03-19 00:46:49 +0600

    Basic model structure of restaurant

---
+++ core/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Models: Restaurant, Rating, Sale
+
+class Restaurant(models.Model):
+    class RestaurantTypes(models.TextChoices):
+        INDIAN = 'IN', 'Indian'
+        CHINESE = 'CH', 'Chinese'
+        ITALIAN = 'IT', 'Italian'
+        GREEK = 'GK', 'Greek'
+        MEXICAN = 'MX', 'Mexican'
+        FASTFOOD = 'FF', 'Fastfood'
+        OTHER = 'OT', 'Other'
+    
+    name = models.CharField(max_length=100)
+    website = models.URLField(default='')
+    date_opened = models.DateField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    restaurant_type = models.CharField(max_length=2, choices=RestaurantTypes.choices)
+
+    def __str__(self):
+        return self.name
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f'{self.restaurant} -- {self.user} -- {self.rating}'
+
+
+# class Sale(models.Model):
+
