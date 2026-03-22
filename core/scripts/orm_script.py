1fc8a24bfbd9ffc766ded22e0ce1c77974f9ef50
Kh-Tanjim-Ashraf <kh.tanjim.ashraf>
2026-03-22 14:50:44 +0600

    Query & fetch the first record from the DB using {model}.objects.all().first() method

--- core/scripts/orm_script.py
+++ core/scripts/orm_script.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 from django.db import connection
 
 def run():
-    restaurants = Restaurant.objects.all() # By default, it fetches total 21 records from the DB.
-    
+    restaurants = Restaurant.objects.all().first() # Fetch the record from the DB.
+
     print(restaurants)
 
     print(connection.queries) # print out all the SQL queries
