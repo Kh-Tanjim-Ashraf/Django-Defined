b17b4147e1a1ec89b7b365a8f39e9b8e5251b0e3
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-05-15 14:24:38 +0600

    Implemented generic CBV createView

--- 2-Django Media Files, Message Framework/product/urls/urls_cbv.py
+++ 2-Django Media Files, Message Framework/product/urls/urls_cbv.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from product.views.views_generic_cbv import ProductListView, ProductDetailView
+from product.views.views_generic_cbv import ProductListView, ProductDetailView, ProductCreateView
 
 app_name = 'CBV_Product'
 
 urlpatterns = [
     path('list/', view=ProductListView.as_view(), name='product-list-CBV'),
     path('list/<int:pk>/', view=ProductDetailView.as_view(), name='product-detail-CBV'),
+    path('create/', view=ProductCreateView.as_view(), name='product-create-CBV'),
 ]
--- 2-Django Media Files, Message Framework/product/views/views_generic_cbv.py
+++ 2-Django Media Files, Message Framework/product/views/views_generic_cbv.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from product.models import Product
+from product.forms.product_model_form import ProductForm
+from django.urls import reverse_lazy
 
 
 class ProductListView(ListView):
@@ -13,3 +16,10 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'product/cbv_templates/detail-product.html'
     context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'product/cbv_templates/create-product.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('CBV_PRODUCT:product-list-CBV')
