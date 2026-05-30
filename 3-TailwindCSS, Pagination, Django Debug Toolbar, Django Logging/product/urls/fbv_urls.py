cd879697c08a387397d210b87f07a43ee14aff70
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-05-30 22:10:10 +0600

    Implement create, update & delete functionalities in FBV

--- 3-TailwindCSS, Pagination, Django Debug Toolbar, Django Logging/product/urls/fbv_urls.py
+++ 3-TailwindCSS, Pagination, Django Debug Toolbar, Django Logging/product/urls/fbv_urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from product.views.func_based_views import productList
+from product.views.func_based_views import productList, productCreate, productUpdate, productDelete
 
 
 urlpatterns = [
     path('list/', view=productList, name='productList-fbv'),
+    path('create/', view=productCreate, name='productCreate-fbv'),
+    path('update/<int:pk>/', view=productUpdate, name='productUpdate-fbv'),
+    path('delete/<int:pk>/', view=productDelete, name='productDelete-fbv'),
 ]
--- 3-TailwindCSS, Pagination, Django Debug Toolbar, Django Logging/product/views/func_based_views.py
+++ 3-TailwindCSS, Pagination, Django Debug Toolbar, Django Logging/product/views/func_based_views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from product.models import Product
 from django.core.paginator import Paginator
 from django.db import connection
+from product.forms.product_model_form import ProductForm
 
 
 def productList(request):
-    products = Product.objects.all().order_by('id') # Best practise for showcasing paginated data; avoids "Unordered Query Odering"
+    products = Product.objects.all().order_by('-id') # Best practise for showcasing paginated data; avoids "Unordered Query Odering"
     paginator = Paginator(products, 10) # Display 5 products each page
     page_num = request.GET.get('page')
     paginated_prods = paginator.get_page(page_num)
@@ -22,5 +23,62 @@ def productList(request):
     return render(
         request=request,
         template_name='./product/fbv/product-list.html',
+        context=context
+    )
+
+
+def productCreate(request):
+    form = ProductForm()
+
+    if request.method == 'POST':
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('ProductApplication:productList-fbv')
+    
+    context = {
+        'form': form
+    }
+    return render(
+        request=request,
+        template_name='./product/fbv/product-create.html',
+        context=context
+    )
+
+
+def productUpdate(request, pk):
+    product = Product.objects.get(id=pk)
+    form = ProductForm(instance=product)
+    
+    if request.method == 'POST':
+        form = ProductForm(data=request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('ProductApplication:productList-fbv')
+    
+    context = {
+        'form': form,
+        'product': product
+    }
+    return render(
+        request=request,
+        template_name='./product/fbv/product-update.html',
+        context=context
+    )
+
+
+def productDelete(request, pk):
+    product = Product.objects.get(id=pk)
+
+    if request.method == 'POST':
+        product.delete()
+        return redirect('ProductApplication:productList-fbv')
+
+    context = {
+        'product': product
+    }
+    return render(
+        request=request,
+        template_name='./product/fbv/product-delete.html',
         context=context
     )
