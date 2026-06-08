80b04c021d2d4be1636aa25ccba0c0d6964c3ae2
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-06-08 13:58:35 +0600

    Integrate Django Debug Toolbar to inspect & optimize Django Application

--- 3-CSS, Pagination, Django Debug Toolbar, Django Logging/core/settings.py
+++ 3-CSS, Pagination, Django Debug Toolbar, Django Logging/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'product.apps.ProductConfig',
     'django_extensions',    # To execute the `runscript` command
+    "debug_toolbar",        # Django Debug Toolbar
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",  # Django Debug Toolbar Middleware
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'core.urls'
--- 3-CSS, Pagination, Django Debug Toolbar, Django Logging/core/urls.py
+++ 3-CSS, Pagination, Django Debug Toolbar, Django Logging/core/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import index
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view=index, name='home-page'),
     path('product/', include(('product.urls.base_urls', 'app_name'), namespace='ProductApplication')),
-]
+] + debug_toolbar_urls()
