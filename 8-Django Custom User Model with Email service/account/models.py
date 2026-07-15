3af7db7cf1f8a50921457bbcd871dfc7f92d8bef
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-07-16 02:58:40 +0600

    F8: Implemented `Custom User Model` w/ minimal configuration

---
+++ 8-Django Custom User Model with Email service/account/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, phone, password, **extra_fields):
+
+        if not email:
+            raise ValueError("Email is required!")
+        
+        if not phone:
+            raise ValueError("Phone is required!")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        # Background Task: Send email to user about successful email creation
+
+        return user
+    
+    def create_superuser(self, email, phone, password, **extra_fields):
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email=email, phone=phone, password=password, **extra_fields)
+
+
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+
+    is_active = models.BooleanField(default=True)
+
+    phone = models.CharField(max_length=17)
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['phone']
+
+    object = UserManager()
+
+    def __str__(self):
+        return self.email
