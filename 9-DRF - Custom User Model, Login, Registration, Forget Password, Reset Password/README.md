4d96390785837997aa21dc7ced0824152aedf2a2
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-07-17 16:09:36 +0600

    F9: Implemented default login & refresh-token API; Added new `Objective` (throttling) in README.md file;

--- 9-DRF - Custom User Model, Login, Registration, Forget Password, Reset Password/README.md
+++ 9-DRF - Custom User Model, Login, Registration, Forget Password, Reset Password/README.md
@@ -9,3 +9,9 @@
 5. Password change API
 6. Froget password API
 7. Reset password API
+8. Email Service: Welcome email, Password reset link
+9. Throttling:
+   - Anonymous User: 20 requests/minute
+   - Authenticated User: 50 requests/minute
+   - Login API: 20 requests/hour
+   - Registration API: 5 requests/day
--- 9-DRF - Custom User Model, Login, Registration, Forget Password, Reset Password/core/settings.py
+++ 9-DRF - Custom User Model, Login, Registration, Forget Password, Reset Password/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'account',
 ]
 
@@ -101,6 +103,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Django Rest Framework (Global Configuration)
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+# DRF Default JWT Configurations
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": True   # `last_login` field in the `auth_user` table is updated upon login (TokenObtainPairView).
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
